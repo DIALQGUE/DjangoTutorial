05896503be78f46ad5888d3fa4f5d147a51eb255
DIALQGUE <fang_za2000@hotmail.co.th>
2022-06-01 16:51:30 +0700

    Used tag for conditional display

--- appSkills/views.py
+++ appSkills/views.py
@@ -1,16 +1,26 @@
+from asyncio.windows_events import NULL
+from ftplib import all_errors
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
-all_skills = [
+allSkills = [
     {'id':1, 'title':'C/C++', 'level':'intermidiate', 'example':'https://github.com/DIALQGUE/Codeforce'},
     {'id':2, 'title':'Go', 'level':'beginner', 'example':'https://github.com/DIALQGUE/Lineman-Wongnai_Intern'},
     {'id':3, 'title':'JavaScript', 'level':'beginner', 'example':'https://github.com/Phyke/SleepyPoker'},
-    {'id':4, 'title':'.NET', 'level':'beginner', 'example':'https://github.com/DIALQGUE/.Net-on-Microsoft-Learn'}
+    {'id':4, 'title':'.NET', 'level':'beginner', 'example':'https://github.com/DIALQGUE/.Net-on-Microsoft-Learn'},
+    {'id':5, 'title':'English', 'level':'practical', 'example': None}
 ]
 
 # Create your views here.
 def skills(request):
-    return render(request, 'appSkills/Skills.html')
+    context = {'skills':allSkills}
+    return render(request, 'appSkills/Skills.html', context)
 
 def skill(request, skillID):
-    return render(request, 'appSkills/Skill.html', context = {'skillID':skillID})
+    selected = None;
+    try:
+        selected = [f for f in allSkills if f['id'] == skillID][0]
+    except IndexError:
+        print('Outo of bound skill')
+    context = {'skill':selected}
+    return render(request, 'appSkills/Skill.html', context)
