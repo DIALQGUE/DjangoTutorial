efeb325e6106d957c39c4bd4a26e2de13fa8e367
DIALQGUE <fang_za2000@hotmail.co.th>
2022-06-02 09:54:41 +0700

    create and connect MySQL database

--- appSkills/models.py
+++ appSkills/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Skill(models.Model):
+    LEVEL = [
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('practical', 'Practical'),
+        ('advanced', 'Advanced'),
+        ('professional', 'Professional')
+    ]
+    
+    title = models.CharField(max_length = 60)
+    level = models.CharField(max_length = 20, choices = LEVEL)
+    example = models.CharField(max_length = 150, null = True)
